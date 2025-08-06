f79e935850dd530de495b70c76724d9ecc37d8c3
Harley Knox <therealharleyknox@gmail.com>
2025-08-06 21:40:34 +0000

    hi, can you help me install my google analytics: Install Google Tag Mana

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
@@ -19,11 +20,23 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-ML7XDP8M');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-ML7XDP8M"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        
         {children}
         <Toaster />
       </body>
