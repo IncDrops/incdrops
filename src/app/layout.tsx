490880b2d90dee2c8be11c658e065fbdfc060250
Harley Knox <therealharleyknox@gmail.com>
2025-08-06 23:37:30 +0000

    thank you. can you set this up as well: Ezoic's JavaScript integration p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
+        <script src="https://cmp.gatekeeperconsent.com/min.js" data-cfasync="false" async></script>
+        <script src="https://the.gatekeeperconsent.com/cmp.min.js" data-cfasync="false" async></script>
+        <script async src="//www.ezojs.com/ezoic/sa.min.js"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.ezstandalone = window.ezstandalone || {};
+          ezstandalone.cmd = ezstandalone.cmd || [];
+        `}} />
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
